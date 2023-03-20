d6fbdb21f01e2e1bf50bbe98012b44025e15ae1c
Mathias Remshardt <remshams@gmail.com>
2023-03-20 14:38:16 +0100

    feat(grid): adds grid with rx observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { interval, map, scan } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'igx-grid-memory';
+  private startTime = new Date();
+
+  initialData = [
+    { key: '1', value: interval(1000).pipe(map(() => new Date())) },
+    { key: '2', value: interval(1000).pipe(map(() => new Date())) },
+    { key: '3', value: interval(1000).pipe(map(() => new Date())) },
+    { key: '4', value: interval(1000).pipe(map(() => new Date())) },
+  ];
+
+  appRunningFor$ = interval(1000).pipe(
+    map(() => new Date().getTime() - this.startTime.getTime())
+  );
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxGridModule } from 'igniteui-angular';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    IgxGridModule,
+    CommonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
